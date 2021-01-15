c01fb072d2321738ba12aa384ed51ddd1cdecf37
alexander-kosov <alxkosov@gmail.com>
2021-01-15 11:31:29 +0400

    2.3 Подключаем к проекту HMR и SSR

--- cfg/webpack.client.config.js
+++ cfg/webpack.client.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 //const htmlWebpackPlugin = require('html-webpack-plugin');
+const {HotModuleReplacementPlugin} = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV;
 const IS_DEV = NODE_ENV==='development';
@@ -12,13 +14,20 @@ function setupDevtool(){
 
 module.exports = {
 	resolve: {
-		extensions: ['.js','.jsx', '.ts', '.tsx', '.json']
+		extensions: ['.js','.jsx', '.ts', '.tsx', '.json'],
+		alias: {
+			'react-dom': IS_DEV ? '@hot-loader/react-dom' : 'react-dom',
+		}
 	},
 	mode: NODE_ENV ? NODE_ENV : 'development',
-	entry: path.resolve(__dirname, '../src/client/index.jsx'),
+	entry: [
+		path.resolve(__dirname, '../src/client/index.jsx'),
+		'webpack-hot-middleware/client?path=http://localhost:3001/static/__webpack_hmr',
+	],
 	output: {
 		path: path.resolve(__dirname, '../dist/client'),
-		filename: 'client.js'
+		filename: 'client.js',
+		publicPath: '/static/'
 	},
 	module: {
 		rules: [
@@ -40,5 +49,9 @@ module.exports = {
 		// open: true,
 		// hot: IS_DEV
 	// },
-	devtool: setupDevtool()
+	devtool: setupDevtool(),
+	plugins: IS_DEV ? [
+		new CleanWebpackPlugin,
+		new HotModuleReplacementPlugin(),
+	] : []
 };
