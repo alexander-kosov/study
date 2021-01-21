930f6ba48061950875fad688320926337e64e0a5
alexander-kosov <alxkosov@gmail.com>
2021-01-21 13:27:51 +0400

    ------

--- dist/client/client.js
+++ dist/client/client.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "df20678cc174be2acac2";
+/******/ 	var hotCurrentHash = "609367fbffed3f29ff5e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
