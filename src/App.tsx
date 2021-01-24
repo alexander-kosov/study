571c33598bf60c2bafb4c55a992f86d82e10f15a
alexander-kosov <alxkosov@gmail.com>
2021-01-24 15:57:31 +0400

    копирование кода урока не заработало, сделал вручную через nanoid()

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,31 @@ import CardsList from './shared/CardsList';
 
 import { MyHooks, useIsMounted } from './HooksExample';
 import {getValue} from '../utils/react/pickFromSyntheticEvent';
+import { generateRandomString, assignId, generateId} from '../utils/react/generateRandomIndex';
+import {MyList} from './shared/GenericList/GenericList';
+
+import { nanoid } from 'nanoid';
+
+
+/* выносится ВНЕ AppComponent, чтобы не перегенерировалась каждый раз */
+const LIST = [
+    {value: 'some'},
+    {value: 'other some'},
+    {value: 'one more some'},
+    {value: 'not some'},
+    {value: 'other not some'}
+//].map((item: {value: string})=>({...item, id: generateRandomString()}));
+//].map(assignId);
+//].map(generateId);
+].map((item:{value:string})=> ({...item, id:nanoid()}));
+
+
+
 
 function AppComponent(){
    // const [isVisible, setIsVisible] = React.useState(false);
     const [title, setTitle] = React.useState('');
-const [isVisible] = useIsMounted();
+    const [isVisible] = useIsMounted();
 
     return (
         <Layout>
@@ -20,6 +40,8 @@ const [isVisible] = useIsMounted();
             <Content>
                 <CardsList />
                 {/* //--------------------------------- */}
+                <MyList list={LIST} />
+                <hr />
                 {/* <button onClick={()=>setIsVisible(!isVisible)}>Change me!</button> */}
                 <input type="text" onChange={getValue(setTitle)}/>
                 {isVisible && <MyHooks title={title} id='11' />}
---
+++ src/shared/GenericList/GenericList.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface IItem {
+    value: string;
+    id: string;
+}
+
+interface IMyListProps {
+    list: IItem[]
+}
+
+export function MyList({list}: IMyListProps) {
+    return (
+        <ul>
+            {list.map((item:IItem,index: number)=>(
+                <li key={item.id}>{item.value}</li>
+            ))}
+        </ul>
+    )
+};
