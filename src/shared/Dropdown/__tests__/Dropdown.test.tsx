4929cb6c3d1711cda8242719af24717e2d36e1b4
alexander-kosov <alxkosov@gmail.com>
2021-01-28 12:16:50 +0400

    enzyme тестирование

--- src/shared/Dropdown/__tests__/Dropdown.test.tsx
+++ src/shared/Dropdown/__tests__/Dropdown.test.tsx
@@ -9,6 +9,7 @@ describe('Dropdown',()=>{
     test('should render',()=>{
     const wrapper = shallow(<Dropdown children={<div />} button={<button />} />)
     expect(wrapper).toBeDefined();
-    expect(wrapper.find('#button')).toBeDefined();
+    //expect(wrapper.find('#button')).toBeDefined();
+    expect(wrapper.find('div.container').isEmptyRender()).toBeFalsy();
     })
 });
