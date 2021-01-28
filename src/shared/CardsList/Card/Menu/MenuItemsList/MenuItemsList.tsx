d9757b3d479f721fbf1c991c191ebe3915a090ef
alexander-kosov <alxkosov@gmail.com>
2021-01-28 14:13:57 +0400

    иконки добавлены в меню

--- src/shared/CardsList/Card/Menu/MenuItemsList/MenuItemsList.tsx
+++ src/shared/CardsList/Card/Menu/MenuItemsList/MenuItemsList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BlockIcon, WarningIcon} from '../../../../Icons';
+import {BlockIcon, WarningIcon, CommentIcon, ShareIcon, SaveIcon} from '../../../../Icons';
 import Text, {EColor} from '../../../../Text';
 //import Text, {EColor} from '../../../../Text/Text';
 import styles from './menuitemslist.less';
@@ -15,10 +15,41 @@ export default function MenuItemsList ({postId}: IMenuItemsList){
        <ul className={styles.menuItemsList}>
             <li className={styles.menuItem} 
                 onClick={()=>console.log(postId)}>
+                <CommentIcon />
+                <Text size={12} tabletSize={14} desktopSize={14}
+                color={EColor.grey99}>
+                Комментарии
+                </Text>
+            </li>
+            <div className={styles.divider} />
+            <li className={styles.menuItem} 
+                onClick={()=>console.log(postId)}>
+                <ShareIcon />
+                <Text size={12} tabletSize={14} desktopSize={14}
+                color={EColor.grey99}>
+                Поделиться
+                </Text>
+            </li>
+
+            <div className={styles.divider} />
+
+            <li className={styles.menuItem} 
+                onClick={()=>console.log(postId)}>
                 <BlockIcon />
-                <Text size={12} tabletSize={14} desktopSize={14} bold
+                <Text size={12} tabletSize={14} desktopSize={14}
                 color={EColor.grey99}>
                 Скрыть
+                </Text>
+            </li>
+
+            <div className={styles.divider} />
+
+            <li className={styles.menuItem} 
+                onClick={()=>console.log(postId)}>
+                <SaveIcon />
+                <Text size={12} tabletSize={14} desktopSize={14}
+                color={EColor.grey99}>
+                Сохранить
                 </Text>
             </li>
 
--- src/shared/Icons/index.ts
+++ src/shared/Icons/index.ts
@@ -1,3 +1,6 @@
 export * from './MenuIcon';
 export * from './BlockIcon';
 export * from './WarningIcon';
+export * from './SaveIcon';
+export * from './ShareIcon';
+export * from './CommentIcon';
