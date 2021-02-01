896970f2bcea53e11f2c20cf43050fc5925c6130
alexander-kosov <alxkosov@gmail.com>
2021-02-01 13:55:09 +0400

    ссылка для авторизации

--- src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
+++ src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
@@ -10,7 +10,9 @@ interface IUserBlockProps {
 
 export default function UserBlock ({avatarSrc, username}: IUserBlockProps){
 	return (
-		<a className={styles.userBox}>
+		<a  href="https://www.reddit.com/api/v1/authorize?client_id=b0t5J709nNhl3Q&response_type=code&
+		state=random_string&redirect_uri=http://localhost:3000/auth&duration=permanent&scope=read submit identity"
+			className={styles.userBox}>
 			<div className={styles.avatarBox}>
 				{avatarSrc ? <img src={avatarSrc} alt="user avatar" className={styles.avatarImage} />
 						   : <AnonIcon />
