70735d4a9d4a63f480a5328806aa7623353e8283
alexander-kosov <alxkosov@gmail.com>
2021-02-04 15:57:13 +0400

    -----

--- src/hooks/usePostsData.ts
+++ src/hooks/usePostsData.ts
@@ -15,7 +15,7 @@ export function usePostsData() {
 
     console.log("#token=",token);
     useEffect(()=>{
-       // if(!token) return;
+        if(!token) return;
         axios.get('https://oauth.reddit.com/best/', {
             headers:{Authorization: `bearer ${token}`}
         })
