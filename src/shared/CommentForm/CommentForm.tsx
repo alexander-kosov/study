a942943de8a9491c617c42ecade3412760f8d1cc
alexander-kosov <alxkosov@gmail.com>
2021-02-08 15:01:42 +0400

    9.3 Controlled и uncontrolled компоненты на примере поля комментария, разница подходов

--- src/shared/CommentForm/CommentForm.tsx
+++ src/shared/CommentForm/CommentForm.tsx
@@ -1,22 +1,23 @@
-import React, { FormEvent, useRef } from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import styles from './commentform.less';
 
-
-
-
 export default function CommentForm (){
-    
-    const ref = useRef<HTMLTextAreaElement>(null);
+   
+   const [value, setValue] = useState(''); 
+
+    function handleChange(event: ChangeEvent<HTMLTextAreaElement>){
+        setValue(event.target.value);
+    }
 
     function handleSubmit(event: FormEvent){
         event.preventDefault();
-        console.log(ref.current?.value);
+        console.log(value);
     }
     
 
     return (
         <form className={styles.form} onSubmit={handleSubmit}>
-            <textarea className={styles.input} ref={ref}/>
+            <textarea className={styles.input} value={value} onChange={handleChange}/>
             <button type="submit" className={styles.button}>Комментировать</button>
 		
 		</form>
