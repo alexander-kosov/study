6370372087ce8f57adf14c1e805cb305e81469fb
alexander-kosov <alxkosov@gmail.com>
2021-02-25 07:52:59 +0400

    13.1 Работа запросами в API ч1

---
+++ dist/client/main.3097eda98939fa83c8ed.hot-update.js
@@ -0,0 +1,27 @@
+webpackHotUpdate("main",{
+
+/***/ "./src/shared/CardsList/CardsList.tsx":
+/*!********************************************!*\
+  !*** ./src/shared/CardsList/CardsList.tsx ***!
+  \********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\r\nvar __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {\r\n    if (k2 === undefined) k2 = k;\r\n    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });\r\n}) : (function(o, m, k, k2) {\r\n    if (k2 === undefined) k2 = k;\r\n    o[k2] = m[k];\r\n}));\r\nvar __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {\r\n    Object.defineProperty(o, \"default\", { enumerable: true, value: v });\r\n}) : function(o, v) {\r\n    o[\"default\"] = v;\r\n});\r\nvar __importStar = (this && this.__importStar) || function (mod) {\r\n    if (mod && mod.__esModule) return mod;\r\n    var result = {};\r\n    if (mod != null) for (var k in mod) if (k !== \"default\" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);\r\n    __setModuleDefault(result, mod);\r\n    return result;\r\n};\r\nvar __importDefault = (this && this.__importDefault) || function (mod) {\r\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\r\n};\r\nObject.defineProperty(exports, \"__esModule\", { value: true });\r\nvar react_1 = __importStar(__webpack_require__(/*! react */ \"./node_modules/react/index.js\"));\r\nvar cardslist_less_1 = __importDefault(__webpack_require__(/*! ./cardslist.less */ \"./src/shared/CardsList/cardslist.less\"));\r\nvar axios_1 = __importDefault(__webpack_require__(/*! axios */ \"./node_modules/axios/index.js\"));\r\nvar react_redux_1 = __webpack_require__(/*! react-redux */ \"./node_modules/react-redux/es/index.js\");\r\n// interface IPostObj {\r\n//     kind: string; \r\n//     data: {\r\n//         id: string;\r\n//         author: string;\r\n//         created_utc: number;\r\n//         num_comments: number;\r\n//         permalink: string;\r\n//         title: string;\r\n//         url: string;\r\n//         thumbnail: string; \r\n//     }\r\n// }\r\nfunction CardsList() {\r\n    var token = react_redux_1.useSelector(function (state) { return state.token; });\r\n    react_1.useEffect(function () {\r\n        if (!token)\r\n            return;\r\n        axios_1.default.get('https://oauth.reddit.com/rising/', {\r\n            headers: { Authorization: \"bearer \" + token }\r\n        })\r\n            .then(function (resp) {\r\n        })\r\n            .catch(function (error) {\r\n        });\r\n    }, [token]);\r\n    // const [posts] = usePostsData();\r\n    // let items = posts.map((post:IPostObj)=>{\r\n    //     return <Card data={post.data} key={post.data.id}/>\r\n    // });\r\n    // return (\r\n    //     <ul className={styles.cardslist}>\r\n    //         {items}\r\n    //     </ul>\r\n    // );\r\n    return (react_1.default.createElement(\"ul\", { className: cardslist_less_1.default.cardslist }));\r\n}\r\nexports.default = CardsList;\r\n\n\n//# sourceURL=webpack:///./src/shared/CardsList/CardsList.tsx?");
+
+/***/ }),
+
+/***/ "./src/shared/CardsList/index.ts":
+/*!***************************************!*\
+  !*** ./src/shared/CardsList/index.ts ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\r\nvar __importDefault = (this && this.__importDefault) || function (mod) {\r\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\r\n};\r\nObject.defineProperty(exports, \"__esModule\", { value: true });\r\nvar CardsList_1 = __importDefault(__webpack_require__(/*! ./CardsList */ \"./src/shared/CardsList/CardsList.tsx\"));\r\nexports.default = CardsList_1.default;\r\n\n\n//# sourceURL=webpack:///./src/shared/CardsList/index.ts?");
+
+/***/ })
+
+})
--- src/shared/CardsList/CardsList.tsx
+++ src/shared/CardsList/CardsList.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './cardslist.less';
 import Card from './Card';
 import { usePostsData } from '../../hooks/usePostsData';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
 
 interface IPostObj {
     kind: string; 
@@ -19,14 +22,45 @@ interface IPostObj {
 
 export default function CardsList (){
 
-    const [posts] = usePostsData();
-    let items = posts.map((post:IPostObj)=>{
-        return <Card data={post.data} key={post.data.id}/>
-    });
+    const token = useSelector<RootState>(state=>state.token);
+    const [posts, setPosts] = useState([]);
+    useEffect(()=>{
 
+        if(!token) return;
+
+        async function load(){
+            try{
+                const {data: {data: {children}}} = await axios.get('https://oauth.reddit.com/rising/',{
+                //const response = await axios.get('https://oauth.reddit.com/rising/',{
+                    headers: {Authorization: `bearer ${token}`}
+                });
+                console.log('response:',children);
+                setPosts(children);
+            } catch (error){
+                console.error(error);
+            }
+        }
+  
+        load(); //!!!!!!!!!!!!!!!
+    },[token]);
+
+
+    // const [posts] = usePostsData();
+    // let items = posts.map((post:IPostObj)=>{
+    //     return <Card data={post.data} key={post.data.id}/>
+    // });
+
+    // return (
+    //     <ul className={styles.cardslist}>
+    //         {items}
+    //     </ul>
+    // );
     return (
-        <ul className={styles.cardslist}>
-            {items}
+         <ul className={styles.cardslist}>
+             {posts.map((post:IPostObj) => {
+               return <Card data={post.data} key={post.data.id}/> 
+             })}
         </ul>
-    );
+    )   
+
 }
