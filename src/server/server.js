a269f09faf0263faa782a59e86dc46325c5aad22
alexander-kosov <alxkosov@gmail.com>
2021-03-03 13:42:04 +0400

    ---------

--- src/server/server.js
+++ src/server/server.js
@@ -14,7 +14,8 @@ app.use('/static', express.static('./dist/client'));
 app.get('/auth',(req, res)=>{
 	res.header("Access-Control-Allow-Origin", "*");
 	axios.post(
-		'https://www.reddit.com/api/v1/access_token',		`grant_type=authorization_code&code=${req.query.code}&redirect_uri=http://demo-redd-skillbox.herokuapp.com:3000/auth`,
+		'https://www.reddit.com/api/v1/access_token',		
+		`grant_type=authorization_code&code=${req.query.code}&redirect_uri=http://demo-redd-skillbox.herokuapp.com:3000/auth`,
 		{
 			auth: {username: process.env.CLIENT_ID, password:process.env.SECRET},
 			headers: {'Content-type': 'application/x-www-form-urlencoded'}
