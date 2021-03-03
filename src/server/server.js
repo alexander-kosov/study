9d4a9b1a760fe71085afe87e0b08c63e017b00ed
alexander-kosov <alxkosov@gmail.com>
2021-03-03 15:25:24 +0400

    ------

--- src/server/server.js
+++ src/server/server.js
@@ -17,7 +17,7 @@ app.get('/auth',(req, res)=>{
 		'https://www.reddit.com/api/v1/access_token',		
 		`grant_type=authorization_code&code=${req.query.code}&redirect_uri=https://demo-redd-skillbox.herokuapp.com/auth`,
 		{
-			auth: {username: process.env.CLIENT_ID, password:process.env.SECRET},
+			auth: {username: process.env.CLIENT_ID, password: process.env.SECRET},
 			headers: {'Content-type': 'application/x-www-form-urlencoded'}
 		}							   
 	)
